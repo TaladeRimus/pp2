97184d660fb2c7dce0f16382e336efdbb45c2437
taladerimus <631310007@SALA503-06.fspoaeduc.com.br>
2015-05-26 09:23:08 -0300

    Nao da pra usar

---
+++ padaria.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE usuario(
+idUsuario SERIAL PRIMARY KEY,
+permissao_usuario integer,
+nome varchar(100),
+endereco varchar(100),
+cpf integer,
+rg integer,
+login varchar(20),
+senha varchar(10)
+);
+
+CREATE TABLE produto (
+idProduto SERIAL PRIMARY KEY,
+nome varchar(100),
+quantidade_total integer,
+preco decimal(10,2)
+);
+
+
+CREATE TABLE venda (
+idVenda SERIAL PRIMARY KEY,
+idUsuarioFK integer,
+idProdutoFK integer,
+quantidade_produto integer
+);
+
+ALTER TABLE venda ADD FOREIGN KEY (idUsuarioFK) REFERENCES usuario(idUsuario);
+ALTER TABLE venda ADD FOREIGN KEY (idProdutoFK) REFERENCES produto(idProduto);
+
+SELECT * FROM venda;
